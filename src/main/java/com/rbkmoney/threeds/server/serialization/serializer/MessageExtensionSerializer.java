f920f3bd572ea9dfeb999c99cbc1d6f7353d23f5
karleowne <a.karlov@rbkmoney.com>
2020-06-18 17:57:13 +0300

    BJ-923: fixation 3ds server state by Mastercard platform tests
    add serialize for messages

--- src/main/java/com/rbkmoney/threeds/server/serialization/serializer/MessageExtensionSerializer.java
+++ src/main/java/com/rbkmoney/threeds/server/serialization/serializer/MessageExtensionSerializer.java
@@ -4,17 +4,5 @@ import com.rbkmoney.threeds.server.domain.message.MessageExtension;
 import com.rbkmoney.threeds.server.serialization.AbstractListSerializer;
 
 public class MessageExtensionSerializer extends AbstractListSerializer<MessageExtension> {
-//
-//    @Override
-//    protected void serializeValue(MessageExtension value, JsonGenerator jsonGenerator) throws IOException {
-//        jsonGenerator.writeStringField("name", value.getName());
-//        jsonGenerator.writeStringField("id", value.getId());
-//        jsonGenerator.writeBooleanField("criticalityIndicator", value.getCriticalityIndicator());
-//
-//        jsonGenerator.writeObjectFieldStart("data");
-//        for (Map.Entry<String, Object> entry : value.getData().entrySet()) {
-//            jsonGenerator.writeObjectField(entry.getKey(), entry.getValue());
-//        }
-//        jsonGenerator.writeEndObject();
-//    }
+
 }
