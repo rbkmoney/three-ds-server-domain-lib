4866591e0cc4b7cbd5c6c1532057b757e258f208
karleowne <a.karlov@rbkmoney.com>
2020-12-04 17:30:11 +0300

    BJ-997: add ThreeDsVersion model

---
+++ src/main/java/com/rbkmoney/threeds/server/domain/versioning/ThreeDsVersion.java
@@ -0,0 +1,22 @@
+package com.rbkmoney.threeds.server.domain.versioning;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonInclude(value = JsonInclude.Include.NON_ABSENT)
+public class ThreeDsVersion {
+
+    private String threeDSServerTransID;
+    private String dsProviderId;
+    private String acsStartProtocolVersion;
+    private String acsEndProtocolVersion;
+    private String dsStartProtocolVersion;
+    private String dsEndProtocolVersion;
+    @ToString.Exclude
+    private String threeDSMethodURL;
+
+}
